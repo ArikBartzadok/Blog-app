d39fdbb8811907fe3b6b74c5b5c0fb96e976c031
Diogo Ferreira <arikbartzadok@gmail.com>
2021-03-06 19:18:01 -0300

    created the model for categories, registration, listing, form validation and flash sessions

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
 const express = require('express')
-const handlebars = require('express')
+const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
+const path = require('path')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // Configs
+// Sessions
+app.use(session({
+    secret: 'blogapp',
+    resave: true,
+    saveUnitialized: true
+}))
+app.use(flash())
+
+// Config middleware
+app.use((req, res, next) => {
+    // Globals vars
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+
+// Body parser
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+// Handlebars
 app.engine('handlebars', handlebars({defaultlayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.set('view engine', '.handlebars')
+
+// Mongoose
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/blogapp').then(() => {
+    console.log('conectado ao mongo');
+}).catch((error) => {
+    console.log('erro ao estabelecer conexõ com o mongo '+error)
+})
+
+// Public static files
+app.use(express.static(path.join(__dirname,'public')))
+
+// Middleware
+app.use((req, res, next) => {
+    console.log('middleware')
+    next()
+})
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('Rota principal')
+})
+app.get('/posts', (req, res) => {
+    res.send('Postagens')
+})
 app.use('/admin', admin)
 
 // Others
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+// Importando os modelos
+require('../models/Categorie')
+const Categories = mongoose.model('categories')
 
 router.get('/', (req, res) => {
-    res.send('Pagina principal')
+    res.render('admin/index')
 })
 
 router.get('/posts', (req, res) => {
@@ -10,7 +15,46 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categories', (req, res) => {
-    res.send('Pagina de categorias')
+    Categories.find().sort({date: 'DESC'}).then((categories) => {
+        res.render('admin/categories', {categories: categories.map(categories => categories.toJSON())})
+    }).catch((error) => {
+        console.log('>>> Erro ao realizar listagem de categorias :: '+error);
+        req.flash('error_msg', 'Ocorreu algum erro ao realizar a listagem de categorias')
+        res.rendirect('/admin')
+    })
+})
+
+router.get('/categories/add', (req, res) => {
+    res.render('admin/categoriesAdd')
+})
+
+router.post('/categories/new', (req, res) => {
+    // Validando formulário
+    var errors = []
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+        errors.push({text: 'Nome inválido'})
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        errors.push({text: 'Nome da categoria é inválido'})
+    }
+    if(errors.length > 0){
+        // ou redirect
+        res.render('admin/categoriesAdd', {errors: errors})
+    }else{
+        const newCategorie = {
+            name: req.body.name,
+            slug: req.body.slug
+        }
+    
+        new Categories(newCategorie).save().then(() => {
+            req.flash('success_msg', 'Categoria cadastrada com sucesso"')
+            res.redirect('/admin/categories')
+        }).catch((error) => {
+            console.log('>>> Erro ao relizar cadastro de categoria :: '+error)
+            req.flash('error_msg', 'Ocorreu um erro ao cadastrar categoria, tente novamente!')
+            res.redirect('/admin')
+        })
+    }
 })
 
 module.exports = router
