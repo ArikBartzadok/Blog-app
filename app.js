fc5f9223d0606b0ee9eded9d4476d633ed93885a
Diogo Ferreira <arikbartzadok@gmail.com>
2021-03-07 18:57:28 -0300

    Routes for accessing and filtering completed posts

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('connect-flash')
 
+// Carregando models
+require('./models/Post')
+const Post = mongoose.model('posts')
+require('./models/Categorie')
+const Categorie = mongoose.model('categories')
+
 // Configs
 // Sessions
 app.use(session({
@@ -36,9 +42,9 @@ app.set('view engine', '.handlebars')
 // Mongoose
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/blogapp').then(() => {
-    console.log('conectado ao mongo');
+    console.log('>>> Conectado ao mongo :)');
 }).catch((error) => {
-    console.log('erro ao estabelecer conexõ com o mongo '+error)
+    console.log('>>> Erro  ao estabelecer conexõ com o mongo :: '+error)
 })
 
 // Public static files
@@ -52,15 +58,69 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index')
+    Post.find().lean().populate('categorie').sort({data: 'DESC'}).then((posts) => {
+        res.render('index', {posts: posts})
+    }).catch((error) => {
+        console.log('>>> Erro interno ao listar publicações :: '+error)
+        req.flash('error_msg', 'Ocorreu algum erro interno durante a listagem das publicações.')
+        req.redirect('/404')
+    })
 })
-app.get('/posts', (req, res) => {
-    res.send('Postagens')
+
+app.get('/post/:slug', (req, res) => {
+    Post.findOne({slug: req.params.slug}).lean().then((post) => {
+        if(post){
+            res.render('post/index', {post: post})
+        }else{
+            req.flash('error_msg', 'Esta postagem não existe!')
+            res.redirect('/')
+        }
+    }).catch((error) => {
+        console.log('>>> Erro interno ao listar publicação :: '+error)
+        req.flash('error_msg', 'Ocorreu algum erro interno ao listar publicação.')
+        res.redirect('/')
+    })
 })
+
+app.get('/categories', (req, res) => {
+    Categorie.find().lean().then((categories) => {
+        res.render('categories/index', {categories: categories})
+    }).catch((error) => {
+        console.log('>>> Erro interno ao listar categorias :: '+error)
+        req.flash('error_msg', 'Ocorreu algum erro interno ao buscar as categorias.')
+        res.redirect('/')
+    })
+})
+
+app.get('/categories/:slug', (req, res) => {
+    Categorie.findOne({slug: req.params.slug}).lean().then((categorie) => {
+        if(categorie){
+            Post.find({categorie: categorie._id}).lean().then((posts) => {
+                res.render('categories/posts', {posts: posts, categorie: categorie})
+            }).catch((error) => {
+                console.log('>>> Erro interno ao listar publicações :: '+error)
+                req.flash('error_msg', 'Ocorreu um erro ao listar as publicações desta categoria.')
+                res.redirect('/')
+            })
+        }else{
+            req.flash('error_msg', 'Esta categoria não existe.')
+            res.redirect('/categories')
+        }
+    }).catch((error) => {
+        console.log('>>> Erro interno ao listar categoria :: '+error)
+        req.flash('error_msg', 'Ocorreu um erro interno ao listar a página desta categoria.')
+        res.redirect('/')
+    })
+})
+
+app.get('/404', (req, res) => {
+    res.send('Ops... página não encontrada!')
+})
+
 app.use('/admin', admin)
 
 // Others
 const PORT = 8080
 app.listen(PORT, () => {
-    console.log('Servidor rodandooo')
+    console.log(`>>> Servidor rodando --port ${PORT}`)
 })
